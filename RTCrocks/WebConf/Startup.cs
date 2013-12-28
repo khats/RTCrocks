b23c1137a9a3d81283888757a78b7fc404d5be89
Alex  Hotyanovich <micropasa@gmail.com>
2013-12-28 04:08:00 +0300

    update

---
+++ RTCrocks/WebConf/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebConf.Startup))]
+namespace WebConf
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
---
+++ RTCrocks/WebConference/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebConference.MvcApplication" Language="C#" %>
